ca1116f163dbd8b05b93f9be76bba194166d9f51
shhrrtnvr <shhrrtnvr@gmail.com>
2020-04-05 04:59:47 +0600

    Python codes added

--- 787-maximum_subsequence_product.py
+++ 787-maximum_subsequence_product.py
@@ -1,3 +1,19 @@
 from sys import stdin, stdout
-a, b, c, d = [int(x) for x in stdin.readline().rstrip().split()]
-print(a)
+
+n = int(input())
+while n != 0:
+    mx = 0
+    dp = 0
+    for v in [int(x) for x in stdin.readline().rstrip().split()]:
+        dp = max(dp + v, v)
+        mx = max(mx, dp)
+    if mx > 0:
+        print("The maximum winning streak is " + str(mx) + ".")
+    else :
+        print("Losing streak.")
+    n = int(input())
+
+
+
+
+
